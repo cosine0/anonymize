3c89b29c680e4e4c2d634b30ead3758b60f45a85
cosine0 <ksitht@gmail.com>
2016-11-09 20:21:52 +0900

    add file selector dialogue to menu button

--- anonist.py
+++ anonist.py
@@ -8,6 +8,9 @@ form_class = uic.loadUiType("main.ui")[0]
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
+        self.input_file_name = None
+        self.output_file_name = None
+
         self.setupUi(self)
 
         # File 메뉴 바인드
@@ -21,24 +24,19 @@ class MyWindow(QMainWindow, form_class):
         self.actionAbout.triggered.connect(self.about_clicked)
 
     def import_clicked(self):
-        QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
-        return QMessageBox().show()
+        self.input_file_name = QFileDialog.getOpenFileName(self, filter='CSV File (.csv)')
 
     def save_input_clicked(self):
-        QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
-        return QMessageBox().show()
+        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (.csv)')
 
     def save_output_clicked(self):
-        QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
-        return QMessageBox().show()
+        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (.csv)')
 
     def help_clicked(self):
-        QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
-        return QMessageBox().show()
+        return QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
 
     def about_clicked(self):
-        QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
-        return QMessageBox().show()
+        return QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
 
 
 if __name__ == '__main__':
