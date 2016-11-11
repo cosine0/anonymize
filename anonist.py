48558f94cbb223b8eaa34908ee34568e28f2445a
cosine0 <ksitht@gmail.com>
2016-11-11 18:28:38 +0900

    implement file import

--- anonist.py
+++ anonist.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
@@ -10,6 +11,7 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()
         self.input_file_name = None
         self.output_file_name = None
+        self.input_data_set = None
 
         self.setupUi(self)
 
@@ -24,19 +26,38 @@ class MyWindow(QMainWindow, form_class):
         self.actionAbout.triggered.connect(self.about_clicked)
 
     def import_clicked(self):
-        self.input_file_name = QFileDialog.getOpenFileName(self, filter='CSV File (.csv)')
+        self.input_file_name = QFileDialog.getOpenFileName(self, filter='CSV File (*.csv)')[0]
+        if not self.input_file_name:
+            return
+        self.input_data_set = get_dataset_from_csv(self.input_file_name)
 
     def save_input_clicked(self):
-        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (.csv)')
+        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (*.csv)')
 
     def save_output_clicked(self):
-        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (.csv)')
+        self.input_file_name = QFileDialog.getSaveFileName(self, filter='CSV File (*.csv)')
 
     def help_clicked(self):
         return QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
 
     def about_clicked(self):
         return QMessageBox.information(self, "Oops", "Not Implemented.", QMessageBox.Ok)
+
+
+class DatasetRecord(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.joined_from = ()
+        self.joined_common_attributes = set()
+        self.has_matched = False
+
+
+def get_dataset_from_csv(file_name):
+    data_set = []
+    with open(file_name) as f:
+        for record in csv.DictReader(f):
+            data_set.append(DatasetRecord(**record))
+    return data_set
 
 
 if __name__ == '__main__':
