a3599bbf2ebf66d651d76cebea6fe9f17b7a772f
cosine0 <ksitht@gmail.com>
2016-11-29 22:27:20 +0900

    불필요한 코드 지움

--- help.py
+++ help.py
@@ -8,8 +8,4 @@ form_class = uic.loadUiType("help.ui")[0]
 class HelpWindow(QMainWindow, form_class):
     def __init__(self):
         super(HelpWindow, self).__init__()
-        self.input_file_name = None
-        # self.output_file_name = None
-        # self.input_data_set = None
-
         self.setupUi(self)
